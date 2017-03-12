82df9fc817d6080a6d6a394e4dca0b6c755d687a
gougouccnu <lishaoweiccnu@163.com>
2017-03-12 11:11:44 +0800

    text

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -3,16 +3,20 @@ var util = require('../../utils/util.js')
 Page({
   data: {
     logs: [],
-    selectedItemPicUrl: "../../resources/pic/2.jpg"
+    selectedItemPicUrl: "../../resources/pic/2.jpg",
+    itemTitle: "香辣烤兔，拍后请联系卖家修改邮费",
+    itemPrice: "57元",
+    itemHaulage: "运费：23元"
   },
   onLoad: function () {
+    selectedItemIndex = wx.getStorageSync('selectedItemIndex')
     this.setData({
       // log数组用map函数转化格式
       //console.log(wx.getStorageSync('logs')) // added by lsw
       logs: (wx.getStorageSync('logs') || []).map(function (log) {
         return util.formatTime(new Date(log))
       }),
-      selectedItemPicUrl: "../../resources/pic/" + wx.getStorageSync('selectedItemIndex') + ".jpg"
+      selectedItemPicUrl: "../../resources/pic/" + selectedItemIndex + ".jpg"
     })
   },
   onShow: function() {
