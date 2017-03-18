575bd6d0a126a2388d5a3aa53bcb1edc2925ff8e
gougouccnu <lishaoweiccnu@163.com>
2017-03-18 09:29:18 +0800

    .

--- pages/haulage/haulage.js
+++ pages/haulage/haulage.js
@@ -1,19 +1,37 @@
-// pages/haulage/haulage.js
+//index.js
+//获取应用实例
+var app = getApp();
+var ifCheckedAll = true;
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+  data: {
+    motto: 'Hello World!!',
+    userInfo: {},
+    ifCheckedAll: true
   },
-  onReady:function(){
-    // 页面渲染完成
+  //事件处理函数
+  checkedAll: function() {
+    ifCheckedAll = ! ifCheckedAll;
+    this.setData({
+      ifCheckedAll: ifCheckedAll
+    })
   },
-  onShow:function(){
-    // 页面显示
+  onLoad: function () {
+    console.log('onLoad')
+    // this值在方法的函数内指向Page，一般用that变量首先捕获this added by lsw
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
   },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
+  // added by lsw 
+  onShareAppMessage: function() {
+    return {
+      title: 'custom share title',
+      path: '/pages/index/index'
+    }
   }
 })
