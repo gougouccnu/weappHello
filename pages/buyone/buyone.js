24400191f8bc59641f030bc65cdedc321f73a15a
gougouccnu <lishaoweiccnu@163.com>
2017-03-22 21:10:48 +0800

    .

--- pages/buyone/buyone.js
+++ pages/buyone/buyone.js
@@ -29,6 +29,7 @@ Page(Object.assign({}, Zan.Quantity, Zan.Toast,{
     totalPrice: 0,
     userInfo: {},
     orderList: [],
+    hasContact: false,
     quantity1: {
       quantity: 1,
       min: 1,
@@ -102,13 +103,23 @@ Page(Object.assign({}, Zan.Quantity, Zan.Toast,{
       console.log('buy from haulage');
       orderList = wx.getStorageSync('orderList');
     }
+
+    var hasContact;
+    var contactsArray = wx.getStorageSync('contactsArray') || [];
+    if (contactsArray.length == 0) {
+      hasContact = false;
+    } else {
+      hasContact = true;
+    }
+    
     // this值在方法的函数内指向Page，一般用that变量首先捕获this added by lsw
     var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
       //更新数据
       that.setData({
-        contacts:contactsArray[1],
+        contacts:contactsArray[0],
+        hasContact: hasContact,
         orderList: orderList,
         totalPrice: caculateTotalPrice(orderList)
       })
