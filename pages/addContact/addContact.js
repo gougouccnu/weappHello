d900d86cbf4fd4f430afb108ca08d297c565ad77
gougouccnu <lishaoweiccnu@163.com>
2017-03-22 23:20:08 +0800

    .

--- pages/addContact/addContact.js
+++ pages/addContact/addContact.js
@@ -15,6 +15,8 @@ function getCityArray(address3Json, selectedArea) {
 
 Page({
   data: {
+    nameInputValue: '',
+    phoneInputValue: '',
     areaRange: [],
     areaIndex: 0,
     cityIndex: 0,
@@ -23,15 +25,28 @@ Page({
     streetRange: ['选择地区']
   },
 
-  onLoad: function () {
+  onLoad: function (options) {
     area = ['选择省份', '北京市', '天津市', '河北省', '山西省', '内蒙古自治区', '辽宁省', '吉林省', '黑龙江省', '上海市', '江苏省', '浙江省', '安徽省', '福建省', '江西省', '山东省', '河南省', '湖北省', '湖南省', '广东省', '广西壮族自治区', '海南省', '重庆市', '四川省', '贵州省', '云南省', '西藏自治区', '陕西省', '甘肃省', '青海省', '宁夏回族自治区', '新疆维吾尔自治区', '台湾省', '香港特别行政区', '澳门特别行政区'];
     var addressJson = require('../common/address3.js');
     address3Json = addressJson.address3();
     console.log(address3Json)
-    
-    this.setData({
-      areaRange: area
-    })
+    console.log(options)
+    if(options.editContact == 'true') {
+      var contactsArray = wx.getStorageSync('contactsArray') || []
+      var contact = contactsArray[parseInt(options.checkItemId)];
+      console.log(contact)
+      if (contactsArray.length > 0) {
+        this.setData({
+          nameInputValue: contact["name"],
+          phoneInputValue: contact["phone"],
+          areaRange: area
+        })
+      }
+    } else {
+      this.setData({
+        areaRange: area
+      })
+    }
   },
 
   onShow: function () {
--- pages/contacts/contacts.js
+++ pages/contacts/contacts.js
@@ -6,7 +6,7 @@ Page({
     contactsArray: []
   },
   checked:function(event){
-    var checkItemId = parseInt(event.target.id);
+    var checkItemId = parseInt(event.currentTarget.id);
     console.log(event);
 
     for(var i=0; i<contactsArray.length; i++) {
@@ -22,6 +22,22 @@ Page({
     })
     //
   },
+  editContact:function(event){
+    var checkItemId = parseInt(event.target.id);
+    console.log(event);
+    wx.navigateTo({
+      url: '../addContact/addContact?editContact=true&checkItemId=' + checkItemId,
+      success: function(res){
+        // success
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
     contactsArray = wx.getStorageSync('contactsArray') || [];
@@ -32,7 +48,7 @@ Page({
   },
   addContats:function(){
     wx.navigateTo({
-      url: '../addContact/addContact'
+      url: '../addContact/addContact?editContact=false'
     })
   },
   onReady:function(){
